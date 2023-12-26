f827e57908017ff611c67a74174202a31c6c6df2
Roger Toledo <rogertoledo28@gmail.com>
2023-12-26 18:37:32 -0300

    fix: add new test case on TestGetLineHash

--- files/file/file_test.go
+++ files/file/file_test.go
@@ -13,6 +13,11 @@ func TestGetLineHash(t *testing.T) {
 			input:       "",
 			expected:    "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
 		},
+		{
+			description: "Retuns the hash of a line",
+			input:       "24770086;0001;08;1;RCPM;02;20160510;00;;;20160510;2621300;4541203,4541206,4619200,4753900,6204000,7020400,7112000,7739099,8020001,8211300;RUA;PADRE CAMARGO;341;;CENTRO;84130000;PR;7735;00;11111111;;;;;;;",
+			expected:    "e7e59d9c340aa2b66d44167ea1ad38a7c4b4a4e8b27f6fd3fbcc5444339ff8e0",
+		},
 	}
 
 	for _, tc := range cases {
